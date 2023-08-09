7ebcb58a4d0c1b69d476458ebabd27257cea9c98
KADEN PARKER <KADEN.PARKER7375@OMAIL.OTECH.EDU>
2023-08-09 12:24:54 -0600

    Completed classes

--- CKK.Logic/Product.cs
+++ CKK.Logic/Product.cs
@@ -1,12 +1,49 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace CKK.Logic
 {
-    internal class Product
+    public class Product
     {
+        private int Id;
+        private string Name;
+        private decimal Price;
+
+
+        public int GetId()
+        {
+            return Id;
+        }
+
+        public void SetId(int id)
+        {
+            Id = id;
+        }
+
+        public string GetName()
+        {
+            return Name;
+        }
+
+        public void SetName(string name)
+        {
+            Name = name;
+        }
+
+        public decimal GetPrice()
+        {
+            return Price;
+        }
+
+        public void SetPrice(decimal price)
+        {
+            Price = price;
+        }
     }
+}
 }
--- CKK.Logic/ShoppingCartItem.cs
+++ CKK.Logic/ShoppingCartItem.cs
@@ -1,12 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace CKK.Logic
 {
-    internal class ShoppingCartItem
+    public class ShoppingCartItem
     {
+        private Product Product;
+        private int Quantity;
+
+        public ShoppingCartItem(Product product, int quantity)
+        {
+            Product = product;
+            Quantity = quantity;
+        }
+
+        public int GetQuantity()
+        {
+            return Quantity;
+        }
+
+        public void SetQuantity(int quantity)
+        {
+            Quantity = quantity;
+        }
+
+        public Product GetProduct()
+        {
+            return Product;
+        }
+
+        public void SetProduct(Product product)
+        {
+            Product = product;
+        }
     }
 }
