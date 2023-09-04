a0fb8eddc56a86e31f4a741010bf52578f81150b
KADEN PARKER <KADEN.PARKER7375@OMAIL.OTECH.EDU>
2023-09-04 15:16:09 -0600

    abstract

---
+++ CKK.Logic/Interfaces/InventoryItem.cs
@@ -0,0 +1,26 @@
+﻿using CKK.Logic.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CKK.Logic.Interfaces
+{
+    public abstract class InventoryItem
+    {
+        private Product product;
+        private int quantity;
+
+        public Product Product 
+        { 
+            get { return product; }
+            set { product = value; }
+        }
+        public int Quantity
+        { 
+            get { return quantity; }
+            set {  quantity = value; } 
+        }
+    }
+}
--- CKK.Logic/Models/Customer.cs
+++ CKK.Logic/Models/Customer.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CKK.Logic.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace CKK.Logic.Models
 {
-    public class Customer
+    public class Customer : Entity
     {
         private int Id;
         private string Name;
